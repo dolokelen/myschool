37075e50c68a5fe1e8b534eb813c365c4b2571c1
dolokelen <dolokelen1@gmail.com>
2023-10-05 21:38:55 +0800

    Create custom user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True, blank=False)
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
